32ccb371b362dc0f33bae94bbdf921a7e3ce28d9
Samisha68 <samishaofficial68@gmail.com>
2025-03-30 22:31:36 +0530

    Initial commit

--- anchor/tests/basic.spec.ts
+++ anchor/tests/basic.spec.ts
@@ -1,16 +1,85 @@
-import * as anchor from '@coral-xyz/anchor'
-import { Program } from '@coral-xyz/anchor'
-import { Basic } from '../target/types/basic'
+import * as anchor from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { Voting } from "../target/types/voting";
+import { describe, it } from "node:test";
+import { expect } from '@jest/globals';
+const IDL = require("../target/idl/voting.json");
+const PROGRAM_ID = new PublicKey(IDL.address);
 
-describe('basic', () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env())
+describe("Voting", async () => {
+  // Initialize these variables at the module level
+  const context = await startAnchor('', [{ name: "voting", programId: PROGRAM_ID }], []);
+  const provider = new BankrunProvider(context);
+  const votingProgram = new anchor.Program<Voting>(
+    IDL,
+    provider,
+  );
 
-  const program = anchor.workspace.Basic as Program<Basic>
+  it("initializes a poll", async () => {
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "What is your favorite color?",
+      new anchor.BN(100),
+      new anchor.BN(1739370789),
+    ).rpc();
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingProgram.programId,
+    );
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+    console.log(poll);
+  });
 
-  it('should run the program', async () => {
-    // Add your test here.
-    const tx = await program.methods.greet().rpc()
-    console.log('Your transaction signature', tx)
-  })
-})
+  it("initializes candidates", async () => {
+    await votingProgram.methods.initializeCandidate(
+      "Pink",
+      new anchor.BN(1),
+    ).rpc();
+    await votingProgram.methods.initializeCandidate(
+      "Blue",
+      new anchor.BN(1),
+    ).rpc();
+    const [pinkAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8), Buffer.from("Pink")],
+      votingProgram.programId,
+    );
+    const pinkCandidate = await votingProgram.account.candidate.fetch(pinkAddress);
+    console.log(pinkCandidate);
+    const [blueAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8), Buffer.from("Blue")],
+      votingProgram.programId,
+    );
+    const blueCandidate = await votingProgram.account.candidate.fetch(blueAddress);
+    console.log(blueCandidate);
+  });
+
+  it("vote candidates", async () => {
+    await votingProgram.methods.vote(
+      "Gulab Jamun",
+      new anchor.BN(1),
+    ).rpc();
+    await votingProgram.methods.vote(
+      "Rasgulla",
+      new anchor.BN(1),
+    ).rpc();
+    await votingProgram.methods.vote(
+      "Sweet Potato",
+      new anchor.BN(1),
+    ).rpc();
+    const [pinkAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8), Buffer.from("Pink")],
+      votingProgram.programId,
+    );
+    const pinkCandidate = await votingProgram.account.candidate.fetch(pinkAddress);
+    console.log(pinkCandidate);
+    const [blueAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8), Buffer.from("Blue")],
+      votingProgram.programId,
+    );
+    const blueCandidate = await votingProgram.account.candidate.fetch(blueAddress);
+    console.log(blueCandidate);
+    expect(blueCandidate.candidateVotes.toNumber()).toBe(1);
+    expect(blueCandidate.candidateName).toBe("Blue");
+  });
+});
